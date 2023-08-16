e429a8259145cd30ab676e5e3f4f0b707a2a4760
jicsontoh <jicsontoh@gmail.com>
2023-08-16 15:33:15 +0800

    Update questions api

---
+++ controllers/questions-controllers.js
@@ -0,0 +1,97 @@
+const HttpError = require("../models/http-error");
+
+const moment = require("moment");
+
+const questions = [
+  {
+    id: "1",
+    title: "qns title",
+    body: "question body",
+    username: "username",
+    gravatar: "image",
+    user_id: "userid1",
+    answer_count: 10,
+    comment_count: 20,
+    views: 27,
+    votes: 10,
+    created_at: "2023/08/14, 13:00",
+    tags: "tags",
+  },
+  {
+    id: "2",
+    title: "qns title2",
+    body: "question body222",
+    username: "username",
+    gravatar: "image",
+    user_id: "userid1",
+    answer_count: 10,
+    comment_count: 20,
+    views: 30,
+    votes: 11,
+    created_at: "2023/07/14, 13:00",
+    tags: "tags",
+  },
+  {
+    id: "3",
+    title: "qns title3",
+    body: "question body333",
+    username: "username2",
+    gravatar: "image",
+    user_id: "userid2",
+    answer_count: 5,
+    comment_count: 10,
+    views: 24,
+    votes: 15,
+    created_at: "2023/06/14, 13:00",
+    tags: "tags",
+  },
+];
+
+const getQuestions = (req, res, next) => {
+  res.json({ questions });
+};
+
+const getSpecificQns = (req, res, next) => {
+  const qnsId = req.params.qid;
+
+  const qns = questions.find((q) => {
+    return q.id === qnsId;
+  });
+
+  if (!qns) {
+    const error = new HttpError("Cannot find such question", 404);
+    return next(error);
+  }
+
+  res.json({ qns });
+};
+
+const postQuestion = (req, res, next) => {
+  const { title, tags, question } = req.body;
+
+  const createdQns = {
+    id: 7,
+    title: title,
+    body: question,
+    username: "username2",
+    gravatar: "image",
+    user_id: "userid2",
+    answer_count: 5,
+    comment_count: 10,
+    views: 24,
+    votes: 15,
+    created_at: moment(),
+    tags: "tags",
+  };
+
+  questions.push(createdQns);
+
+  res.status(201).json({ createdQns });
+};
+
+const updateQuestion = (req, res, next) => {};
+
+exports.getQuestions = getQuestions;
+exports.getSpecificQns = getSpecificQns;
+exports.postQuestion = postQuestion;
+exports.updateQuestion = updateQuestion;
--- routes/questions-routes.js
+++ routes/questions-routes.js
@@ -1,10 +1,15 @@
 const express = require("express");
 
+const qnsController = require("../controllers/questions-controllers");
+
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  console.log("GET");
-  res.send("QUESTIONS");
-});
+router.get("/", qnsController.getQuestions);
+
+router.get("/:qid", qnsController.getSpecificQns);
+
+router.post("/new", qnsController.postQuestion);
+
+router.patch("/:qid", qnsController.updateQuestion);
 
 module.exports = router;
