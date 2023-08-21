f8a6c8605189e585e328cb0ad43093872226af4c
jicsontoh <jicsontoh@gmail.com>
2023-08-21 14:28:22 +0800

    Handle CORS error

--- app.js
+++ app.js
@@ -14,6 +14,17 @@ dotenv.config();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+  next();
+});
+
 app.use("/api/users", usersRoutes);
 
 app.use("/api/questions", questionsRoutes);
